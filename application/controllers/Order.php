d08a3c7d4e55251ff1fe1e39018a298a4776aeea
Gerald Becker <ger_0_1@hotmail.com>
2015-10-16 21:07:07 -0700

    Fixed the welcome page and removed errors. Implemented start new order functionality. Fixed Order display and order additions which was the ability to add to the cart, updating the order value and being able to checkout the cart.

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -18,7 +18,15 @@ class Order extends Application {
     // start a new order
     function neworder() {
         //FIXME
-
+        $order_num = $this->orders->highest() + 1;
+        
+        $neworder = $this->orders->create();
+        $neworder->num = $order_num;
+        $neworder->date = date();
+        $neworder->status = 'a';
+        $neworder->total = 0;
+        $this->orders->add($neworder);
+        
         redirect('/order/display_menu/' . $order_num);
     }
 
@@ -30,6 +38,8 @@ class Order extends Application {
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
         //FIXME
+        $this->data['title'] = "Order # ".$order_num.' ('. 
+                number_format($this->orders->total($order_num), 2) . ')';;
 
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
@@ -63,13 +73,14 @@ class Order extends Application {
     
     // make a menu ordering column
     function make_column($category) {
-        //FIXME
+        $items = $this->menu->some('category', $category);
         return $items;
     }
 
     // add an item to an order
     function add($order_num, $item) {
         //FIXME
+        $this->orders->add_item($order_num, $item);
         redirect('/order/display_menu/' . $order_num);
     }
 
@@ -78,14 +89,22 @@ class Order extends Application {
         $this->data['title'] = 'Checking Out';
         $this->data['pagebody'] = 'show_order';
         $this->data['order_num'] = $order_num;
-        //FIXME
-
+        
+        $this->data['total'] = number_format($this->orders->total($order_num), 2);
+        
+        $items = $this->orderitems->group($order_num);
+        foreach($items as $item) {
+            $menuitem = $this->menu->get($item->item);
+            $item->code = $menuitem->name;
+        }
+        $this->data['items'] = $items;
+        
         $this->render();
     }
 
     // proceed with checkout
     function proceed($order_num) {
-        //FIXME
+        $this->data['okornot'] = $this->orders->validate($num);
         redirect('/');
     }
 
